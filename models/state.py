34adabd21287c96b09e80da50c5e296a42cff236
Michael Ibrahim <segunmichael24@gmail.com>
2023-02-19 22:07:38 +0100

    Web-static 0-index and other AirBnB files

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" module for State class """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    initiation of State that inherits from BaseModel class
+
+    Public Class Attributes:
+       (string) name: initialized as empty string
+    """
+    name = ""
